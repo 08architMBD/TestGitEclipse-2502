ef5b53dc68562b975be4f3039f1c249e93432a9e
08architMBD <architsaxena@gmail.com>
2022-02-25 16:04:25 +0530

    Test

---
+++ using arrays/src/co/archit/Program01.java
@@ -0,0 +1,12 @@
+package co.archit;
+
+import java.util.Arrays;
+
+public class Program01 {
+	public static void main(String[] args) {
+		int[] nums = {12,76,99,76};
+		System.out.println("nums ="+Arrays.toString(nums));
+		System.out.println("nums contains" + nums.length + "integers");
+	}
+
+}
---
+++ using arrays/src/co/archit/p2.java
@@ -0,0 +1,15 @@
+package co.archit;
+
+public class p2 {
+	
+		public static void main(String[] args) {
+			int[] nums ;
+			nums=new int[]{12,76,99,76};
+			for (int ii = 0; ii < nums.length; ii++) {
+				System.out.println(nums[ii]);
+				
+			}
+
+}
+	}
+
